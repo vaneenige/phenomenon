9ce6c0c21df86e2a2477ffa423786aa20b2e805e
Colin van Eenige <cvaneenige@gmail.com>
2018-07-27 13:05:30 +0200

    Add support for Edge.

--- src/index.js
+++ src/index.js
@@ -211,14 +211,14 @@ class Renderer {
   /**
    * Create a renderer.
    * @param {HTMLElement} canvas - The element on which the scene will be rendered.
-   * @param {object} [context={}] - Options used when getting the context.
-   * @param {string} [contextType='webgl'] - The context identifier defining the drawing context.
-   * @param {object} [settings={}] - Options used when creating the renderer.
+   * @param {object} context - Options used when getting the context.
+   * @param {string} contextType - The context identifier defining the drawing context.
+   * @param {object} settings - Options used when creating the renderer.
    */
   constructor({
     canvas = document.querySelector('canvas'),
     context = {},
-    contextType = 'webgl',
+    contextType = 'experimental-webgl',
     settings = {},
   } = {}) {
     // Get context with optional parameters
